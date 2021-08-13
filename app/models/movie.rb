9f47d1f3328c914423b4e50202ecef2407c66012
Rzt Prog <rztprog@outlook.fr>
2021-08-13 11:41:53 +0200

    Add movie modl + routes + seed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  has_many :lists
 
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Lists
+
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # get "lists/:id", to: "lists#show"
+  # post "lists", to: "lists#create"
+
+  # get "bookmarks/new", to: "bookmarks#new"
+  # post "bookmarks", to: "bookmarks#create"
+
+  # Bookmarks
+  resources :lists, only: [ :index, :show, :create, :new ] do
+    
+    resources :bookmarks, only: [ :new, :create ]
+  end
+
+  resources :bookmarks, only: :destroy
 end
