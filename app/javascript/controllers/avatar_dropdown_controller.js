84326022662291e82db029082ae6fef8c4684e5c
Rzt Prog <rztprog@outlook.fr>
2022-05-20 17:55:06 +0200

    Custom navbar

--- app/javascript/controllers/avatar_dropdown_controller.js
+++ app/javascript/controllers/avatar_dropdown_controller.js
@@ -1,8 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["dropdown"];
 
   connect() {
-    console.log("Hello from our first Stimulus controller")
+    console.log("Avatar dropdown stimulus controller connected !")
+  }
+
+  action() {
+    console.log("Click on dropdown")
+    this.dropdownTarget.classList.toggle("d-none");
   }
 }
