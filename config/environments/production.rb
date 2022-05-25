958c896f1208a94d4a3efb49a8807b09139247e1
Rzt Prog <rztprog@outlook.fr>
2022-05-25 16:03:42 +0200

    Add action mail default url to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: 'https://rzt-rails-watch-list.herokuapp.com/' }
+  # config.action_mailer.default_url_options = { host: 'https://rzt-rails-watch-list.herokuapp.com/', port: 80 }
 end
