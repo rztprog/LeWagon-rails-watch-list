3edbd83e9265e12faba2409b7e2cce0596f61653
Rzt Prog <rztprog@outlook.fr>
2022-05-27 18:09:15 +0200

    gst add button to able to delete own list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -35,6 +35,12 @@ class ListsController < ApplicationController
     redirect_to list_path(@list)
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to root_path
+  end
+
   private
 
   def set_list
