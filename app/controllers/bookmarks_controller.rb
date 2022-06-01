29041186a876e9b561b68ecd6b05f15f6815e63d
Rzt Prog <rztprog@outlook.fr>
2022-06-01 12:11:43 +0200

    Add bookmark pundit policy and setup

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,11 +3,13 @@ class BookmarksController < ApplicationController
 
   def new
     @bookmark = Bookmark.new
+    authorize @bookmark
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
+    authorize @bookmark
 
     if @bookmark.save
       redirect_to list_path(@list)
@@ -18,6 +20,7 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
     @bookmark.destroy
     redirect_to list_path(@list)
   end
